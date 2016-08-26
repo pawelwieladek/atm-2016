1bf8dfac19cda0ef49847298698614540c1ac776
Pawel Wieladek <pawel.wieladek@allegrogroup.com>
2016-08-26 16:51:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ATM 2016
+
+## Front-end Empire State Building
+
+[http://allegro.tech/meeting/](Allegro Tech Meeting)
---
+++ index.js
@@ -0,0 +1,16 @@
+Reveal.initialize({
+    dependencies: [
+        // Interpret Markdown in <section> elements
+        { src: 'bower_components/reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+        { src: 'bower_components/reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+
+        // Syntax highlight for <code> elements
+        { src: 'bower_components/reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+
+        // Zoom in and out with Alt+click
+        { src: 'bower_components/reveal.js/plugin/zoom-js/zoom.js', async: true },
+
+        // Speaker notes
+        { src: 'bower_components/reveal.js/plugin/notes/notes.js', async: true }
+    ]
+});
